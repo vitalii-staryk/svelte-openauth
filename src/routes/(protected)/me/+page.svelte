01984a3c7efc34ead529c8181d5bfe0ca7ac0943
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2025-06-26 18:49:21 +0300

    Update +page.svelte

--- src/routes/(protected)/me/+page.svelte
+++ src/routes/(protected)/me/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
   import {onMount} from "svelte";
 
-  let res: string = '';
+  let res = '';
+  let manualRes = '';
 
   onMount(() => {
-    fetch('https://test-servre-open-auth-production.up.railway.app/protect').then((resp) => resp.json().then(json => res = json) )
+    fetch('https://test-servre-open-auth-production.up.railway.app/protect', { credentials: 'include' })
+      .then((resp) => resp.json().then(json => {
+        res = JSON.stringify(json);
+      }))
   })
+
+  async function fetchManual() {
+    const resp = await fetch('https://test-servre-open-auth-production.up.railway.app/protect', { credentials: 'include' });
+    manualRes = JSON.stringify(await resp.json());
+  }
 </script>
 
 <div>
   <h1>Athorized</h1>
-  <p>{res}</p>
+  <p>onMount: {res}</p>
+  <button on:click={fetchManual}>Fetch /protect вручну</button>
+  <p>Manual fetch: {manualRes}</p>
 </div>
