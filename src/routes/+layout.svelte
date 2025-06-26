a76a0ceaa80e252eec5fddf366fdce485f56326e
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2025-06-26 19:19:32 +0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,17 @@
   import {goto} from "$app/navigation";
   import {browser} from "$app/environment";
 
-  const checkJwt = () => {
-    let success = false
-    fetch('https://test-servre-open-auth-production.up.railway.app/protect', { credentials: 'include' }).then(() => { success = true; });
+  const checkJwt = async () => {
+  let success = false;
+  try {
+    const resp = await fetch('https://test-servre-open-auth-production.up.railway.app/protect', { credentials: 'include' });
+    if (resp.ok) success = true;
+  } catch {}
 
-    if (!success) {
-      goto('/', {
-        replaceState: true,
-      });
-    } else {
-      goto('/me', {
-        replaceState: true,
-      });
-    }
-  };
+  if (!success && window.location.pathname !== '/') {
+    goto('/', { replaceState: true });
+  }
+};
 
   $: if (browser) {
     checkJwt();
